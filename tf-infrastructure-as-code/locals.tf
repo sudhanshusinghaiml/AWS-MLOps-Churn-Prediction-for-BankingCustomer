9e8965f230130c3ee245540e6e872974bc2ffa4c
learn-modern-technology <mailmeatss2112@gmail.com>
2023-09-15 18:30:10 +0530

    Removing important resources details

---
+++ tf-infrastructure-as-code/locals.tf
@@ -0,0 +1,18 @@
+locals {
+  code_build_service_role      = "arn:aws:iam::959999474169:role/service-role/banking-customer-churn-codebuild-service-role"
+  code_pipeline_role           = "arn:aws:iam::959999474169:role/service-role/AWSCodePipelineServiceRole-us-east-1-churn_pipeline"
+  ecs_service_role             = "arn:aws:iam::959999474169:role/AWS-ECS-Service-Role"
+  ecs_task_role                = "arn:aws:iam::959999474169:role/ECSTaskExecutionRole"
+  vpc_id                       = "vpc-006a117f95111f259"
+  subnets = [
+    "subnet-0911024c785757682",
+    "subnet-05189d7bb4490ce61",
+    "subnet-0d1692e706f5ba52a",
+    "subnet-03fdb4e1e28453ff1",
+    "subnet-091adbb37f77e64a7",
+    "subnet-0709706a350f530e9"
+  ]
+  tags = {
+    application = "banking-customer-churn-application"
+  }
+}
